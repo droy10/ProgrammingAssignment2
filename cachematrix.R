efd80d7f3394ba9b62e82fdc407b4832fcc248d4
Daniel Roy <danielRoy10@hotmail.com>
2019-08-16 15:54:31 -0400

    Initial Changes on Aug 16

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions show how the operand "<<-" can be used to change the scope of variables.
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that caches a matrix inverse, and retrieves it without re-calculating it, if it is already 
+## cached.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## To run, type, for example, "cacheSolve(makeCacheMatrix(matrix(1:4, 2, 2)))"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# To run, type, for example, "cacheSolve(makeCacheMatrix(matrix(1:4, 2, 2)))"
+  print("I call makeCacheMatrix the first time, the data won't be cached:")
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  message("not getting cached data")
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  print(m)
+  print("I call makeCacheMatrix the second time, the data will be cached:")
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  message("not getting cached data")
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
